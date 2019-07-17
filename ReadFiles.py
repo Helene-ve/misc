b6f6cc651219d00787c83d49e38743e715764795
Helene-ve <51315935+Helene-ve@users.noreply.github.com>
2019-07-17 23:14:53 +0200

    Update ReadFiles.py

--- ReadFiles.py
+++ ReadFiles.py
@@ -5,16 +5,21 @@ Created on Wed Jul 17 13:34:55 2019
 @author: Helene
 """
 
+import os
 def convert_date(timestamp):
     d = datetime.utcfromtimestamp(timestamp)
-    formatted_date = d.strftime('%d %b %Y')
+    formatted_date = d.strftime('%m_%d_%Y')
     return formatted_date
 
 from datetime import datetime
 def get_files():
-    dir_entries = os.scandir('/media/moelven/Apis/01/')
+    dir_entries = os.scandir('YourDataPath')
     for entry in dir_entries:
         if entry.is_file():
             info = entry.stat()
-            print(f'{entryname}\t Last Modified: {convert_date(info..st_mtime)})
+            datum_modified = convert_date(info.st_mtime)
+            print(f'{entry.name}\t Last Modified: ', datum_modified)
+    
+get_files()
 
+print('Program has executed')
